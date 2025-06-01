2cc64f420b294fb2e1a3dbba9690f90f3b8ca842
Pedro Amaral <amaral1@ufpr.br>
2025-06-01 13:05:55 -0300

    feat: Implementa função diametros

--- grafo.c
+++ grafo.c
@@ -334,7 +334,7 @@ unsigned int bipartido(grafo *g)
                         cor[idx_vizinho] = 1 - cor[atual];
                         fila[fila_fim++] = idx_vizinho;
                     }
-                    
+
                     // Se falhar em algum dos casos, já não é bipartido 
                     else if (cor[idx_vizinho] == cor[atual]) 
                     {
@@ -353,4 +353,128 @@ unsigned int bipartido(grafo *g)
 
     free(cor);
     return 1;
+}
+
+static void busca_largura_distancia(grafo *g, int inicio, int *dist, int *visitado) 
+{
+    unsigned int *fila = malloc(g->num_vertices * sizeof(unsigned int));
+    unsigned int ini = 0, fim = 0;
+
+    fila[fim++] = inicio;
+    dist[inicio] = 0;
+    visitado[inicio] = 1;
+
+    while (ini < fim) 
+    {
+        int atual = fila[ini++];
+        vertice *vizinho = g->lista_adj[atual]->prox;
+
+        while (vizinho) 
+        {
+            int idx = -1;  // Inicializa com um valor padrão para 'não encontrado'
+            for (unsigned int i = 0; i < g->num_vertices; i++) 
+            {
+                if (strcmp(g->lista_adj[i]->nome, vizinho->nome) == 0) 
+                {
+                    idx = i;
+                    break;
+                }
+            }
+
+            if (idx != -1 && !visitado[idx]) 
+            {
+                dist[idx] = dist[atual] + 1;
+                visitado[idx] = 1;
+                fila[fim++] = idx;
+            }
+            vizinho = vizinho->prox;
+        }
+    }
+
+    free(fila);
+}
+
+// Usa a estratégia de usar duas buscas em largura para encontrar o diâmetro do componente
+char *diametros(grafo *g) 
+{
+    int *visitado_global = calloc(g->num_vertices, sizeof(int));
+    int *diametros = malloc(g->num_vertices * sizeof(int));
+    int qtd_diametros = 0;
+
+    for (unsigned int i = 0; i < g->num_vertices; i++) 
+    {
+        if (visitado_global[i]) 
+            continue;
+
+        // 1ª busca a partir de um vértice qualquer
+        int *dist1 = calloc(g->num_vertices, sizeof(int));
+        int *visit1 = calloc(g->num_vertices, sizeof(int));
+        busca_largura_distancia(g, i, dist1, visit1);
+
+        int max_dist = 0, idx_max = i;
+        for (unsigned int j = 0; j < g->num_vertices; j++) 
+        {
+            if (dist1[j] > max_dist) 
+            {
+                max_dist = dist1[j];
+                idx_max = j;
+            }
+
+            if (visit1[j])
+                visitado_global[j] = 1;
+        }
+
+        free(dist1);
+        free(visit1);
+
+        // 2ª busca a partir do vértice encontrado na 1ª
+        int *dist2 = calloc(g->num_vertices, sizeof(int));
+        int *visit2 = calloc(g->num_vertices, sizeof(int));
+        busca_largura_distancia(g, idx_max, dist2, visit2);
+
+        int diametro = 0;
+        for (unsigned int j = 0; j < g->num_vertices; j++) 
+        {
+            if (dist2[j] > diametro)
+                diametro = dist2[j];
+        }
+
+        diametros[qtd_diametros++] = diametro;
+
+        free(dist2);
+        free(visit2);
+    }
+
+    free(visitado_global);
+
+    // Ordena de forma decrescente
+    for (int i = 0; i < qtd_diametros - 1; i++) 
+    {
+        for (int j = i + 1; j < qtd_diametros; j++) 
+        {
+            if (diametros[i] > diametros[j]) 
+            {
+                int tmp = diametros[i];
+                diametros[i] = diametros[j];
+                diametros[j] = tmp;
+            }
+        }
+    }
+
+    // Concatena tudo
+    char *res = malloc(12 * qtd_diametros + 1);
+    res[0] = '\0';
+    char buffer[16];
+
+    for (int i = 0; i < qtd_diametros; i++) 
+    {
+        sprintf(buffer, "%d", diametros[i]);
+        strcat(res, buffer);
+
+        if (i < qtd_diametros - 1)
+            strcat(res, " ");
+    }
+
+    free(diametros);
+    return res;
 }
