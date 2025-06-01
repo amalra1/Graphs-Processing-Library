2a03dfcc29c786cc1ad5d74d25763aabfc5a70a3
Pedro Amaral <amaral1@ufpr.br>
2025-06-01 11:39:33 -0300

    refactor: Altera identação

--- teste.c
+++ teste.c
@@ -3,32 +3,33 @@
 #include "grafo.h"
 
 //------------------------------------------------------------------------------
-int main(void) {
+int main(void) 
+{
 
-  grafo *g;
-  char *s;
+	grafo *g;
+	char *s;
 
-  g = le_grafo(stdin);
+	g = le_grafo(stdin);
 
-  printf("Nome do grafo lido: %s\n", nome(g));
-  printf("%d vertices\n", n_vertices(g));
-  printf("%d arestas\n", n_arestas(g));
+	printf("Nome do grafo lido: %s\n", nome(g));
+	printf("%d vertices\n", n_vertices(g));
+	printf("%d arestas\n", n_arestas(g));
 
-  // Pra testar
-  imprime_lista_adjacencia(g);
+	// printf("%d componentes\n", n_componentes(g));
 
-  // printf("%d componentes\n", n_componentes(g));
+	// printf("%sbipartido\n", bipartido(g) ? "" : "não ");
 
-  // printf("%sbipartido\n", bipartido(g) ? "" : "não ");
+	// printf("diâmetros: %s\n", s=diametros(g));
+	// free(s);
 
-  // printf("diâmetros: %s\n", s=diametros(g));
-  // free(s);
+	// printf("vértices de corte: %s\n", s=vertices_corte(g));
+	// free(s);
 
-  // printf("vértices de corte: %s\n", s=vertices_corte(g));
-  // free(s);
+	// printf("arestas de corte: %s\n", s=arestas_corte(g));
+	// free(s);
 
-  // printf("arestas de corte: %s\n", s=arestas_corte(g));
-  // free(s);
+	// Pra testar
+	imprime_lista_adjacencia(g);
 
-  return ! destroi_grafo(g);
+	return ! destroi_grafo(g);
 }
